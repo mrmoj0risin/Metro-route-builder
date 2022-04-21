362ac1aea017eb1a446e25ccd6196a5ea27059ee
me <st@m-max.ru>
2022-04-21 13:08:14 +0300

    Homework is done

--- ExceptionsDebuggingAndTesting/homework_1/ConsoleCustomerList/src/main/java/CustomerStorage.java
+++ ExceptionsDebuggingAndTesting/homework_1/ConsoleCustomerList/src/main/java/CustomerStorage.java
@@ -1,5 +1,6 @@
 import java.util.HashMap;
 import java.util.Map;
+import java.util.function.Function;
 
 public class CustomerStorage {
     private final Map<String, Customer> storage;
@@ -15,23 +16,35 @@ public class CustomerStorage {
         final int INDEX_PHONE = 3;
 
         String[] components = data.split("\\s+");
+        if (components.length != 4){
+            throw new IllegalArgumentException("Wrong format. Correct format:" +
+                    " add Василий Петров vasily.petrov@gmail.com +79215637722");
+        }
         String name = components[INDEX_NAME] + " " + components[INDEX_SURNAME];
         storage.put(name, new Customer(name, components[INDEX_PHONE], components[INDEX_EMAIL]));
     }
 
     public void listCustomers() {
+        if (storage.isEmpty()){ throw new RuntimeException("List is empty");}
         storage.values().forEach(System.out::println);
     }
 
     public void removeCustomer(String name) {
-        storage.remove(name);
+        if (storage.containsValue(name)) {
+            storage.remove(name);
+        }
+        else { throw new RuntimeException("No customer with name - " + name);}
     }
 
     public Customer getCustomer(String name) {
-        return storage.get(name);
+        if (storage.containsValue(name)) {
+            return storage.get(name);
+        }
+        else { throw new RuntimeException("No customer with name - " + name);}
     }
 
     public int getCount() {
         return storage.size();
     }
+
 }
--- ExceptionsDebuggingAndTesting/homework_1/ConsoleCustomerList/src/main/java/Main.java
+++ ExceptionsDebuggingAndTesting/homework_1/ConsoleCustomerList/src/main/java/Main.java
@@ -17,18 +17,28 @@ public class Main {
             String command = scanner.nextLine();
             String[] tokens = command.split("\\s+", 2);
 
-            if (tokens[0].equals("add")) {
-                executor.addCustomer(tokens[1]);
-            } else if (tokens[0].equals("list")) {
-                executor.listCustomers();
-            } else if (tokens[0].equals("remove")) {
-                executor.removeCustomer(tokens[1]);
-            } else if (tokens[0].equals("count")) {
-                System.out.println("There are " + executor.getCount() + " customers");
-            } else if (tokens[0].equals("help")) {
-                System.out.println(helpText);
-            } else {
-                System.out.println(COMMAND_ERROR);
+            try
+            {
+                if (tokens[0].equals("add")) {
+                    executor.addCustomer(tokens[1]);
+                } else if (tokens[0].equals("list")) {
+                    executor.listCustomers();
+                } else if (tokens[0].equals("remove")) {
+                    if (tokens.length != 2){ throw new RuntimeException(COMMAND_ERROR); }
+                    executor.removeCustomer(tokens[1]);
+                } else if (tokens[0].equals("count")) {
+                    System.out.println("There are " + executor.getCount() + " customers");
+                } else if (tokens[0].equals("help")) {
+                    System.out.println(helpText);
+                } else {
+                    System.out.println(COMMAND_ERROR);
+                }
+            }
+            catch (ArrayIndexOutOfBoundsException | IllegalArgumentException ex){
+                System.out.println(ex.getMessage());
+            }
+            catch (RuntimeException exception){
+                System.out.println(exception.getMessage());
             }
         }
     }
